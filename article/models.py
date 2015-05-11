1c9595a2d18b8e70ae09d801106793ff526be747
pangpang <pptemprdbms@gmail.com>
2015-05-10 22:59:32 -0700

    database created

--- article/models.py
+++ article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model) :
+    title = models.CharField(max_length = 100)  #blog title
+    category = models.CharField(max_length = 50, blank = True)  #blog tag
+    date_time = models.DateTimeField(auto_now_add = True)  #blog date
+    content = models.TextField(blank = True, null = True)  #bnlog article
+
+    def __unicode__(self) :
+        return self.title
+
+    class Meta:  #sort by created date
+        ordering = ['-date_time']
