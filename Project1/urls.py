3c7383fcc9021bdaa717b78a8bf1546b3ce3b85c
Pang Pang <pptemprdbms@gmail.com>
2015-05-11 10:44:52 -0700

    revert url import setting

--- Project1/urls.py
+++ Project1/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
